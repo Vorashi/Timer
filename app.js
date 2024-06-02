ae5e64d7a38c537dc697fdea2b4427280c51994e
Матвей Шаров <matvejsarov673@gmail.com>
2024-06-02 19:58:49 +0300

    Вроде не плохо получилось, можно и по другому сделать, но пока лень (готовый вариант)

--- app.js
+++ app.js
@@ -6,27 +6,34 @@ let hours = 0;
 let minutes = 0;
 let seconds = 0;
 let startTimer;
+let timerIsRunning = false;
 
-btnStart.addEventListener('click', () => startTimer = setInterval( () => {
-		seconds++;
-		if (seconds > 60) {
-			seconds = 0;
-			minutes++;
-		}
-		if (minutes > 60) {
-			minutes = 0;
-			hours++;
-		}
-		viewTimer.innerText = `${(hours < 10) ? 0 : ''}${hours} : ${(minutes < 10) ? 0 : ''}${minutes} : ${(seconds < 10) ? 0 : ''}${seconds}`
-	}, 1000) 
-);
+btnStart.addEventListener('click', () => { 
+	if (!timerIsRunning) {
+		timerIsRunning = true;
+		startTimer = setInterval(() => {
+			seconds++;
+			if (seconds > 60) {
+				seconds = 0;
+				minutes++;
+			}
+			if (minutes > 60) {
+				minutes = 0;
+				hours++;
+			}
+			viewTimer.innerText = `${(hours < 10) ? 0 : ''}${hours} : ${(minutes < 10) ? 0 : ''}${minutes} : ${(seconds < 10) ? 0 : ''}${seconds}`
+		}, 1000) 
+	}	
+});
 
 btnStop.addEventListener('click', () => {
   clearInterval(startTimer);
+	timerIsRunning = false;
 });
 
 btnReset.addEventListener('click', () => {
   clearInterval(startTimer);
+	timerIsRunning = false;
 	hours = 0;
 	minutes = 0;
 	seconds = 0;
